3f28e56c53a10fc7b8fbe778e35d954260485f80
Chirchir-Dan <1danchirchir@gmail.com>
2024-11-07 15:35:17 +0300

    fixed pycodestyle issues

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,17 +6,23 @@ This module contains a function to obfuscate specific fields in log messages.
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """
-    Replaces the values of specified fields in a log message with a redaction string.
+    Replaces the values of specified fields in a log message with
+    a redaction string.
 
     Args:
         fields (List[str]): List of field names to obfuscate.
         redaction (str): The string to replace each field's value with.
         message (str): The original log message.
-        separator (str): The character that separates fields in the message.
+        separator (str): The character that separates fields
+        in the message.
 
     Returns:
         str: The log message with specified fields obfuscated.
     """
-    return re.sub(f'({"|".join(fields)})=([^;{separator}]+)', lambda m: f"{m.group(1)}={redaction}", message)
+    return re.sub(
+        f'({"|".join(fields)})=([^;{
+            separator}]+)', lambda m: f"{m.group(1)}={redaction}", message)
