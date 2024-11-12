d70e206228311f51405a66039f79daa40fc2072f
Chirchir-Dan <1danchirchir@gmail.com>
2024-11-12 16:16:04 +0300

    added error handler route for status code 401

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
+def unauthorized_access() -> str:
+    """raises a 401 Unauthorized error"""
+    abort(401)
