6983aa517ebe75ad0de41691bbd15f1b4cb6df35
Chirchir-Dan <1danchirchir@gmail.com>
2024-11-21 16:02:03 +0300

    fixed pycodestyle issues

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -49,6 +49,7 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            # If the user doesn't exist, hash the password and create the new user
+            # If the user doesn't exist, hash the password and create the new
+            # user
             hashed_password = self._hash_password(password)
             return self._db.add_user(email, hashed_password.decode('utf-8'))
